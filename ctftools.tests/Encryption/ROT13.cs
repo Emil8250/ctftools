c7c706d6f0d455dc59837671c03d305b37c2fc73
Emil Olesen <emil.olesen@twoday.com>
2023-10-13 15:49:39 +0200

    Changed ROT13 to use linq

--- ctftools.tests/Encryption/ROT13.cs
+++ ctftools.tests/Encryption/ROT13.cs
@@ -9,9 +9,15 @@ namespace ctftools.tests.Encryption
     public class ROT13
     {
         [Fact]
-        public void RotateShouldReturnRotatedString()
+        public void RotateShouldReturnRotatedStringBy13()
         {
             Assert.Equal("Th1sIzFlag{f14g}", ctftools.Encryption.ROT13.Rotate("Gu1fVmSynt{s14t}"));
+        }
+
+        [Fact]
+        public void RotateShouldReturnRotatedStringByN()
+        {
+            Assert.Equal("Th1sIzFlag{f14g}", ctftools.Encryption.ROT13.Rotate("Dr1cSjPvkq{p14q}", 16));
         }
     }
 }
--- src/Encryption/ROT13.cs
+++ src/Encryption/ROT13.cs
@@ -5,37 +5,24 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace ctftools.Encryption
+namespace ctftools.Encryption;
+
+public class ROT13
 {
-    public class ROT13
+    /// <summary>
+    /// A simple cipher that rotates letters by 13.
+    /// </summary>
+    /// <param name="input">The string input which needs to be rotated</param>
+    /// <param name="n">The amount of spaces to rotate</param>
+    /// <returns></returns>
+    public static string Rotate(string input, int n = 13)
     {
-        /// <summary>
-        /// A simple cipher that rotates letters by 13.
-        /// </summary>
-        /// <param name="input">The string input which needs to be rotated</param>
-        /// <param name="n">The amount of spaces to rotate</param>
-        /// <returns></returns>
-        public static string Rotate(string input, int n = 13)
+        return string.Concat(input.Select(c =>
         {
-            return string.Concat(input.Select(c =>
-            {
-                if (!char.IsLetter(c))
-                    return c;
+            if (!char.IsLetter(c)) return c;
+            var baseChar = char.IsUpper(c) ? 'A' : 'a';
+            return (char)(baseChar + (c - baseChar + n) % 26);
 
-                var rightRot = (char)(c + n);
-                var leftRot = (char)(c - n);
-
-                if (char.IsLetter(rightRot) && char.IsLetter(leftRot))
-                {
-                    return char.IsUpper(c)
-                        ? (char.IsUpper(leftRot) ? leftRot : rightRot)
-                        : (char.IsLower(leftRot) ? leftRot : rightRot);
-                }
-
-                return char.IsLetter(rightRot) ? rightRot : leftRot;
-            }));
-        }
+        }));
     }
 }
-
-//public static string Rot13(string input) => Regex.Replace(input, "[a-zA-Z]", new MatchEvaluator(c => ((char)(c.Value[0] + (Char.ToLower(c.Value[0]) >= 'n' ? -13 : 13))).ToString()));
