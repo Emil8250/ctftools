15af9da78e9a9a4dfd7ee8272e3bc7d4af501e39
Emil Olesen <e-olesen@hotmail.com>
2023-10-13 15:10:28 +0200

    Initial commit

---
+++ ctftools.tests/Encryption/ROT13.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ctftools.tests.Encryption
+{
+    public class ROT13
+    {
+        [Fact]
+        public void RotateShouldReturnRotatedString()
+        {
+            Assert.Equal("Th1sIzFlag{f14g}", ctftools.Encryption.ROT13.Rotate("Gu1fVmSynt{s14t}"));
+        }
+    }
+}
---
+++ src/Encryption/ROT13.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ctftools.Encryption
+{
+    public class ROT13
+    {
+        /// <summary>
+        /// A simple cipher that rotates letters by 13.
+        /// </summary>
+        /// <param name="input">The string input which needs to be rotated</param>
+        /// <param name="n">The amount of spaces to rotate</param>
+        /// <returns></returns>
+        public static string Rotate(string input, int n = 13)
+        {
+            var result = "";
+            foreach (var c in input)
+            {
+                char rightRot = Convert.ToChar(c + n);
+                char leftRot = Convert.ToChar(c - n);
+                if (!char.IsLetter(c))
+                {
+                    result += c.ToString();
+                    continue;
+                }
+                if (char.IsLetter(rightRot) && char.IsLetter(leftRot))
+                {
+                    if (char.IsUpper(c))
+                    {
+                        if (char.IsUpper(leftRot))
+                            result += leftRot;
+                        else
+                            result += rightRot;
+                    }
+                    else
+                    {
+                        if (char.IsLower(leftRot))
+                            result += leftRot;
+                        else
+                            result += rightRot;
+                    }
+                }
+                else
+                {
+                    if (char.IsLetter(rightRot))
+                        result += rightRot;
+                    else if (char.IsLetter(leftRot))
+                        result += leftRot;
+                }
+
+            }
+            return result;
+        }
+    }
+}
+
+//public static string Rot13(string input) => Regex.Replace(input, "[a-zA-Z]", new MatchEvaluator(c => ((char)(c.Value[0] + (Char.ToLower(c.Value[0]) >= 'n' ? -13 : 13))).ToString()));
