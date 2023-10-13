4bcfbee99142a2bbfb49d6dd882fad3fd61cb304
Emil Olesen <e-olesen@hotmail.com>
2023-10-13 21:29:09 +0200

    Converting binary string to text

---
+++ ctftools.tests/Format/Binary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ctftools.tests.Encryption
+{
+    public class Binary
+    {
+        [Fact]
+        public void ToTextShouldConvertBinaryStringToText()
+        {
+            var flag = "01100110 01101100 01100001 01100111 01111011 00110001 00110010 00110011 01111101";
+            Assert.Equal("flag{123}", ctftools.Format.Binary.ToText(flag));
+        }
+    }
+}
---
+++ src/Format/Binary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ctftools.Format;
+public class Binary
+{
+    public static string ToText(string inputBytes)
+    {
+        return string.Concat(inputBytes.Split(" ")
+            .Where(bin => !string.IsNullOrEmpty(bin))
+            .Select(bin => Convert.ToChar(Convert.ToInt32(bin, 2))));
+    }
+}
