2abc2ffb8db12cdcaba213db83a916629c237bcc
Emil Olesen <e-olesen@hotmail.com>
2023-10-13 22:31:40 +0200

    Added support for binary strings

--- ctftools.tests/Format/Binary.cs
+++ ctftools.tests/Format/Binary.cs
@@ -8,10 +8,11 @@ namespace ctftools.tests.Encryption
 {
     public class Binary
     {
-        [Fact]
-        public void ToTextShouldConvertBinaryStringToText()
+        [Theory]
+        [InlineData("01100110 01101100 01100001 01100111 01111011 00110001 00110010 00110011 01111101")]
+        [InlineData("011001100110110001100001011001110111101100110001001100100011001101111101")]
+        public void ToTextShouldConvertBinaryStringToText(string flag)
         {
-            var flag = "01100110 01101100 01100001 01100111 01111011 00110001 00110010 00110011 01111101";
             Assert.Equal("flag{123}", ctftools.Format.Binary.ToText(flag));
         }
     }
--- src/Format/Binary.cs
+++ src/Format/Binary.cs
@@ -10,7 +10,20 @@ public class Binary
 {
     public static string ToText(string inputBytes)
     {
-        return string.Concat(inputBytes.Split(" ")
+        var bytes = inputBytes.Split(" ");
+        bool addSpace = bytes.Length == 1 && inputBytes.Length != 8;
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < inputBytes.Length; i++)
+        {
+            if (addSpace && i % 8 == 0)
+            {
+                result.Append(" ");
+            }
+            result.Append(inputBytes[i]);
+        }
+
+        return string.Concat(result.ToString().Split(" ")
             .Where(bin => !string.IsNullOrEmpty(bin))
             .Select(bin => Convert.ToChar(Convert.ToInt32(bin, 2))));
     }
