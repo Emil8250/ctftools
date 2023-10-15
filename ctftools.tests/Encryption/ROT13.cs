62308795a84e28ccaa0cc623968fdd0001d11f98
nichlasolesen <=>
2023-10-15 20:17:27 +0200

    added a new methods that allows for numbers to also be rotated

--- ctftools.tests/Encryption/ROT13.cs
+++ ctftools.tests/Encryption/ROT13.cs
@@ -19,5 +19,17 @@ namespace ctftools.tests.Encryption
         {
             Assert.Equal("Th1sIzFlag{f14g}", ctftools.Encryption.ROT13.Rotate("Dr1cSjPvkq{p14q}", 16));
         }
+
+        [Fact]
+        public void RotateShouldReturnRotatedStringIncludingNumbersBy13()
+        {
+            Assert.Equal("Th1sIzFlag{f14g}", ctftools.Encryption.ROT13.Rotate("Gu8fVmSynt{s81t}", true));
+        }
+
+        [Fact]
+        public void RotateShouldReturnRotatedStringIncludingNumbersByN()
+        {
+            Assert.Equal("Th1sIzFlag{f14g}", ctftools.Encryption.ROT13.Rotate("Dr5cSjPvkq{p58q}", true, 16));
+        }
     }
 }
