6658b5c0e9bdfb470d2a312a62605855ebb9571d
Terri Penn <coderterri@protonmail.com>
2023-10-15 10:43:43 -0500

    Added format methods for encoding/decoding base64, and associated tests.

---
+++ ctftools.tests/Format/Base64.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ctftools.tests.Format
+{
+	public class Base64
+	{
+        [Theory]
+        [InlineData("ZmxhZ3tiYXNlNjR9", "flag{base64}")]
+		[InlineData("VGVzdCE=", "Test!")]
+		[InlineData("", "")]
+		public void ToTextShouldConvertBase64StringToText(string base64, string expected)
+		{
+            Assert.Equal(expected, ctftools.Format.Base64.ToText(base64));
+		}
+
+        [Theory]
+        [InlineData("flag{base64}", "ZmxhZ3tiYXNlNjR9")]
+		[InlineData("Test!", "VGVzdCE=")]
+        [InlineData("", "")]
+        public void ToBase64ShouldConvertTextToBase64String(string text, string expected)
+		{
+			Assert.Equal(expected, ctftools.Format.Base64.ToBase64(text));
+		}
+	}
+}
